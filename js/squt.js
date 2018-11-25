3535ebd697ce361d4d11b847e42d3efe625c7908
Bruno Perel <brunoperel@gmail.com>
2018-11-25 15:29:12 +0100

    Handle table and column aliases Show node centers And layout constraints : table/column are X aligned, table/table alias are Y aligned, table alias/column alias are X aligned

--- js/squt.js
+++ js/squt.js
@@ -6,43 +6,57 @@ const d3cola = cola.d3adaptor(d3)
     .avoidOverlaps(true)
     .size([width, height]);
 
-const svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+const svg = d3.select("body").append("svg");
 
 const groupPadding = 3;
+const columnTopPadding = 0;
 
 let graph = {};
 
 const color = d3.scaleOrdinal(d3.schemeCategory20);
 
+const tables = [],
+      columns = [],
+      tableAliases = [],
+      columnAliases = [];
+
 d3.json("parsed.json", function (error, response) {
 
     function addNode(title, type, data) {
         const nodeId = graph.nodes.length;
-        graph.nodes.push(Object.assign({}, data, {id: nodeId, name: title, type: type}));
+
+        data.width = 200//60 + title.length * 16;
+
+        if (type === 'table-alias') {
+            const relatedNodes = graph.nodes.filter(function (otherNode) {
+                return (data.table === otherNode.table || data.alias === otherNode.table) && otherNode.type !== "table-alias"
+            });
+            data.height = d3.sum(relatedNodes, function(relatedNode) { return 40 });
+        }
+        else {
+            data.height = 40;
+        }
+        graph.nodes.push(Object.assign({}, {id: nodeId, name: title, type: type}, data));
         return nodeId;
     }
 
-    const tables = [],
-          columns = [],
-          aliases = [];
     response.statements.forEach(function(statement) {
         statement.from.forEach(function(from) {
             if (from.table) {
                 tables.push(from);
-                aliases.push({table: from.table, alias: from.alias})
+                tableAliases.push({table: from.table, alias: from.alias})
             }
         });
         statement.join.forEach(function(join) {
             if (join.expr) {
                 tables.push(join.expr);
-                aliases.push({table: join.expr.table, alias: join.expr.alias})
+                tableAliases.push({table: join.expr.table, alias: join.expr.alias});
             }
         });
         statement.expr.forEach(function(expr) {
             if (expr.table) {
-                columns.push(expr)
+                columns.push(expr);
+                columnAliases.push(expr)
             }
         });
     });
@@ -54,20 +68,11 @@ d3.json("parsed.json", function (error, response) {
         const tableNodeId = addNode(table.table, "table-title", table);
 
         const columnGroupId = graph.groups.length;
-        graph.groups.push({id: tableNodeId + "_columns", leaves: [tableNodeId]});
+        graph.groups.push({id: tableNodeId + "_columns", leaves: [tableNodeId], name: table.table});
 
         graph.groups.push({id: tableNodeId + "_aliases", leaves: []});
 
         graph.groups.push({id: tableNodeId, leaves: [], groups: [columnGroupId]});
-    });
-
-    aliases.forEach(function(alias) {
-        const aliasNodeId = addNode(alias.alias, "table-alias", alias);
-        const relatedTable = graph.nodes.filter(function(node) { return node.type === "table-title" && node.name === alias.table})[0];
-        graph.groups.filter(function(group) { return group.id === relatedTable.id + "_aliases"})[0].leaves.push(aliasNodeId);
-        graph.constraints.push(
-            {type: "alignment", axis: "y", offsets: [{node: relatedTable.id, offset: 0}, {node: aliasNodeId, offset: 0}]},
-            {axis: "x", left: relatedTable.id, right: aliasNodeId, equality: "true", gap: 100});
     });
 
     columns.forEach(function(column){
@@ -77,38 +82,48 @@ d3.json("parsed.json", function (error, response) {
         const columnNodeId = addNode(column.column, "table-column", column);
         relatedNodes.forEach(function(relatedNode) {
             graph.groups.filter(function(group) { return group.id === relatedNode.id + "_columns"})[0].leaves.push(columnNodeId);
+
+            // The column and the table name should be X-aligned
+            graph.constraints.push(
+                {type: "alignment", axis: "x", offsets: [{node: relatedNode.id, offset: 0}, {node: columnNodeId, offset: 0}]});
         });
+    });
+
+    tableAliases.forEach(function(tableAlias) {
+        const tableAliasNodeId = addNode(tableAlias.alias, "table-alias", tableAlias);
+        const relatedTable = graph.nodes.filter(function(node) { return node.type === "table-title" && node.name === tableAlias.table})[0];
+        graph.groups.filter(function(group) { return group.id === relatedTable.id + "_aliases"})[0].leaves.push(tableAliasNodeId);
+
+        // The table alias and the table name should be Y-aligned
+        graph.constraints.push(
+            {type: "alignment", axis: "y", offsets: [{node: relatedTable.id, offset: 0}, {node: tableAliasNodeId, offset: 0}]});
+    });
+
+    columnAliases.forEach(function(columnAlias) {
+        const columnAliasNodeId = addNode(columnAlias.alias, "column-alias", columnAlias);
+        const relatedAliasTable = graph.nodes.filter(function(node) { return node.type === "table-alias" && node.name === columnAlias.table})[0];
+        const relatedTable = graph.nodes.filter(function(node) { return node.type === "table-title" && node.name === relatedAliasTable.table})[0];
+        graph.groups.filter(function(group) { return group.id === relatedTable.id + "_aliases"})[0].leaves.push(columnAliasNodeId);
+
+        // The column alias and the table alias should be X-aligned
+        graph.constraints.push(
+            {type: "alignment", axis: "x", offsets: [{node: relatedAliasTable.id, offset: 0}, {node: columnAliasNodeId, offset: 0}]});
     });
 
     graph.links = [];
 
-    graph.nodes.forEach(function (v) {
-        if (v.type === 'table-title') {
-            v.width = 200;
-        }
-        else {
-            v.width = 60 + v.name.length * 16;
-        }
-        if (v.type === 'table-alias') {
-            const relatedNodes = graph.nodes.filter(function (otherNode) {
-                return (v.table === otherNode.table || v.alias === otherNode.table) && otherNode.type !== "table-alias"
-            });
-            v.height = d3.sum(relatedNodes, function(relatedNode) { return 40});
-        }
-        else {
-            v.height = 40;
-        }
-    });
     d3cola
         .nodes(graph.nodes)
         .links(graph.links)
         .groups(graph.groups)
         .constraints(graph.constraints)
+        .avoidOverlaps(true)
         .start(10, 10, 10);
 
     var group = svg.selectAll(".group")
         .data(graph.groups)
         .enter().append("rect")
+        .attr("id", function(d) { return d.id;})
         .attr("class", "group table")
         .attr("rx", 8).attr("ry", 8)
         .style("fill", function (d, i) { return color(i); })
@@ -133,15 +148,18 @@ d3.json("parsed.json", function (error, response) {
         })
         .attr("rx", 5).attr("ry", 5);
 
+    const nodeCenters = svg.selectAll(".node-center")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("class", function (d) {
+            return "node-center " + d.type;
+        })
+        .attr("r", 5);
+
     const label = svg.selectAll(".label")
         .data(graph.nodes)
         .enter().append("text")
         .attr("class", "label")
-        .text(function (d) {
-            return d.name;
-        });
-
-    node.append("title")
         .text(function (d) {
             return d.name;
         });
@@ -159,7 +177,7 @@ d3.json("parsed.json", function (error, response) {
                 return d.bounds.width();
             })
             .attr("height", function (d) {
-                return d.bounds.height();
+                return d.bounds.height() + columnTopPadding ;
             });
 
         link
@@ -181,7 +199,15 @@ d3.json("parsed.json", function (error, response) {
                 return d.x + groupPadding - d.width / 2;
             })
             .attr("y", function (d) {
-                return d.y + groupPadding - d.height / 2;
+                return d.y + groupPadding + columnTopPadding - d.height / 2;
+            });
+
+        nodeCenters
+            .attr("cx", function (d) {
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                return d.y;
             });
 
         label
@@ -190,7 +216,7 @@ d3.json("parsed.json", function (error, response) {
             })
             .attr("y", function (d) {
                 const h = this.getBBox().height;
-                return d.y + h / 4;
+                return d.y + columnTopPadding + h / 4;
             });
     });
 });
